06139feab417659b0aebaaa3a78a0321ffb835d5
mjoppich <github@mjoppich.net>
2022-07-03 00:41:08 +0200

    variable service port

--- python/server/start_server.sh
+++ python/server/start_server.sh
@@ -5,11 +5,13 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASE=${1:-/mnt/w/miRExplore_pmid_pmc/}
 PMIDSENTS=${2:-/mnt/w/PubMed/}
 PMCSENTS=${3:-/mnt/w/PubMedCentral/}
+SERVICEPORT=${4:-65500}
 
 echo $BASE
 echo $PMIDSENTS
 echo $PMCSENTS
+echo $SERVICEPORT
 
 sudo service mongodb start
 
-python3 $CURDIR/mirexplore_server_fast.py --textmine $BASE --obodir $BASE/obodir/ --sentdir $PMIDSENTS --feedback $BASE/obodir/feedback --port 65500 --sentdir-pmc $PMCSENTS --load-pmc
+python3 $CURDIR/mirexplore_server_fast.py --textmine $BASE --obodir $BASE/obodir/ --sentdir $PMIDSENTS --feedback $BASE/obodir/feedback --port $SERVICEPORT --sentdir-pmc $PMCSENTS --load-pmc
